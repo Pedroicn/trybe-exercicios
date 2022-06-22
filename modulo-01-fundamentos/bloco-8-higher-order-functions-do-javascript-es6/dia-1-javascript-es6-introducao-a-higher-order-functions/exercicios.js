0b46ed078dfdfee8f1f9e820a7538e6370ed3cd9
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-22 11:06:27 -0300

    Exercicio 2

--- modulo-01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
+++ modulo-01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
@@ -1,3 +1,4 @@
+// EXERCÍCIO 1
 const newRegister = (fullName) => {
   const getEmail = fullName.toLowerCase().split(' ').join('_');
   const cadastro = {name: fullName, email: `${getEmail}@trybe.com`};
@@ -12,4 +13,24 @@ const newEmployees = () => {
   }
   return employees;
 };
-console.log(newEmployees())
+console.log(newEmployees());
+
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+
+function compareNumbers(number, ramdomNumber) {
+  
+  if (ramdomNumber === number) {
+    return 'Tente novamente'
+  } else {
+    return 'Parabéns você ganhou!'
+  }
+}
+
+
+function numbersDraw(number, callback) {
+  const ramdomNumber = Math.floor(Math.random() * 5 + 1);
+  return callback(number, ramdomNumber); 
+}
+
+console.log(numbersDraw(3, compareNumbers))
