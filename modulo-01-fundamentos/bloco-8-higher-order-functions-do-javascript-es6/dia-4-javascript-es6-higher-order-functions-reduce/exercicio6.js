32bd61a51be9972e38eaf8ce4d7fe73d5daac29c
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-28 11:28:39 -0300

    Exercicio 6

--- modulo-01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
+++ modulo-01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
@@ -4,11 +4,8 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  const media = grades.map((grade) => grade.reduce((acc, crr) => (acc + crr)) / grade.length);
+  const obj = students.map((student, index) => ({name: student, average: media[index]}))
+  return obj;
 }
-
-const expected = [
-  { name: 'Pedro Henrique', average: 7.8 },
-  { name: 'Miguel', average: 9.2 },
-  { name: 'Maria Clara', average: 8.8 },
-];
+console.log(studentAverage());
