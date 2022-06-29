84de32c827c02dc3e579ae9f1f0b36a81bc51c67
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-29 09:11:37 -0300

    Exercicio 6

--- modulo-01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio6.js
+++ modulo-01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio6.js
@@ -4,3 +4,7 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+function toObject(modelo, marca, ano) {
+  return {modelo, marca, ano}
+}
+console.log(toObject(...palio));
