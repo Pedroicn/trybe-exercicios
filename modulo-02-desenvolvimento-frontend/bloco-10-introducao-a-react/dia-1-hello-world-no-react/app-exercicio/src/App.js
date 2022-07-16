193c6c14a84a62f5ecacab6e4914953d16110524
Pedro Nunes <pedro.icn88@gmail.com>
2022-07-16 18:31:35 -0300

    Exercicio 1 dia 10.1

--- modulo-02-desenvolvimento-frontend/bloco-10-introducao-a-react/dia-1-hello-world-no-react/app-exercicio/src/App.js
+++ modulo-02-desenvolvimento-frontend/bloco-10-introducao-a-react/dia-1-hello-world-no-react/app-exercicio/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
-import Task from './componentes/task';
-// exercicio 1
-const array = ['JAVASCRIPT', 'CSS', 'REACT', 'HTML']
-const elem = array.map((element) => Task(element))
+// import Task from './componentes/task';
+// // exercicio 1
+// const array = ['JAVASCRIPT', 'CSS', 'REACT', 'HTML']
+// const elem = array.map((element) => Task(element))
+// function App() {
+//   return (
+//      elem
+//   );
+// }
+
+// export default App;
+import Hearder from './componentes/Header';
+import Content from './componentes/Content';
+import Footer from './componentes/Footer';
 function App() {
   return (
-     elem
+    <div>
+      <Hearder/>
+      <Content/>
+      <Footer/>
+    </div>
+     
   );
 }
 
---
+++ modulo-02-desenvolvimento-frontend/bloco-10-introducao-a-react/dia-1-hello-world-no-react/app-exercicio/src/componentes/Footer.js
@@ -0,0 +1,7 @@
+function Footer() {
+  const text = "E isso é só o começo...";
+  return (
+    <h1 className="footer">{text}</h1>
+  )
+}
+export default Footer
