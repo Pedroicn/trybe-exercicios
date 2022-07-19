a486937fb40be5544cc86106a22b53e455f9fb02
Pedro Nunes <pedro.icn88@gmail.com>
2022-07-19 16:15:28 -0300

    Exercicio Finalizado

--- modulo-02-desenvolvimento-frontend/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ modulo-02-desenvolvimento-frontend/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import pokemons from './data';
+import './pokemoncss.css'
+import Pokedex from './components/Pokedex';
 function App() {
   return (
     <div className="App">
-      
+      <Pokedex pokemon={pokemons}/>
     </div>
   );
 }
